a05753cd90b5bf7a6525fba1c94a54cb88f4f3e1
Rossco <rosshiggins@rocketmail.com>
2019-10-11 16:05:55 +0100

    added FolderWatch

--- src/app/portfolio-view/portfolio-view.model.ts
+++ src/app/portfolio-view/portfolio-view.model.ts
@@ -5,29 +5,17 @@ export class Repos {
         {
           imagePath: "./assets/Images/medventory.png", 
           title: "MedVentory", 
-          problemTitle: "The Problem: ", 
-          solutionTitle: "The Solution: ",
-          problemText: "Tracking of Interventional Radiology procedures & stock used was still being done via pen and paper. Messy and prone to error.",
-          solutionText: "MedVentory - reduces the amount of manually work required & automatically keeps track of the stock used across procedures, thus making ordering easier & more accurate. Bonus feature: no illegible 'Dr's handwriting' to worry about!",
           tags: ["C#.Net","WPF"],
           repo: "https://github.com/MeetMyCode/MedVentory"
         },
         {
           imagePath: "./assets/Images/cctvHeaderImage.png", 
           title: "FolderWatch", 
-          problemTitle: "The Problem: Auto-generated statistical results could not be passed on to the relevent people in a timely manner due to a short-coming of the statistical application.", 
-          solutionTitle: "The Solution: Monitor the output folder of the statisical application and email the output to the relevant recipients.",
-          problemText: "Keeping tabs on Parliamentary MP's can be difficult.",
-          solutionText: "Political See  - puts all the information at your fingertips. It's a dashboard to quickly view almost anything about any of the MP's currently within the UK Parlaiment.",
-          tags: ["iOS - Swift"]
+          tags: ["C#.Net","WPF"]
         },    
         {
           imagePath: "./assets/Images/PlaceholderImage.png", 
           title: "HydroMonitor", 
-          problemTitle: "The Problem: ", 
-          solutionTitle: "The Solution: ",
-          problemText: "Keeping track of hydroponic nutrient parameters required manual measuring.",
-          solutionText: "HydroMonitor  -  automates nutrient parameter measurements with values viewable via app.",
           tags: ["Web"]
         }       
       
